eb9cec1330f43310269f462110624b0fbd824e91
robmuh <rwx@robs.io>
2019-12-18 21:32:22 -0500

    save

--- doc.go
+++ doc.go
@@ -1,5 +1,5 @@
 /*
-Package timefmt enables easy human input of times, dates, and durations. It also includes many convenience functions for rouding time duration boundaries as
+Package htime enables easy human input of times, dates, and durations. It also includes many convenience functions for rouding time duration boundaries as
 is frequently needed for scheduling and time-based search applications.
 
 Pointers to time.Time are used throughout the package since <nil> is usually a more desirable zero value than that for time.Time without a pointer.
